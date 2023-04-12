28f99808f56ac9385b8649e45916874b5c39640d
senakuwabara <piisena.978@gmail.com>
2023-04-12 20:14:51 +0900

    フッター修正中

--- script.js
+++ script.js
@@ -1,26 +1,39 @@
-const swiper = new Swiper(".swiper", {
-    // ページネーションが必要なら追加
-    pagination: {
-      el: ".swiper-pagination"
-    },
-    // ナビボタンが必要なら追加
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev"
-    }
-  });
 
-//scroll_effect
-  $(function () {
-    $(window).scroll(function () {
-      const windowHeight = $(window).height();
-      const scroll = $(window).scrollTop();
-  
-      $('.scroll-up').each(function () {
-        const targetPosition = $(this).offset().top;
-        if (scroll > targetPosition - windowHeight + 100) {
-          $(this).addClass("is-fadein");
-        }
+//swiper
+const swiper = new Swiper('.swiper', {
+  slidesPerView: 1,
+  loop: true,
+  speed: 1000,
+  autoplay: {
+    delay: 1000,
+  },
+  pagination: {
+    el: ".swiper-pagination"
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev"
+  },
+  breakpoints: {
+    767: {
+      slidesPerView: 2,
+    }
+  }
+});
+
+//アコーディオンにクラス付与する
+$(".accordion-button").on("click", function(){
+  $(".accordion-button").addClass("collapsed");
+});
+
+//フェードイン
+$(window).scroll(function (){
+  $('.fadein').each(function(){
+      var elemPos = $(this).offset().top,
+          scroll = $(window).scrollTop(),
+          windowHeight = $(window).height();
+        if (scroll > elemPos - windowHeight + 100){
+            $(this).addClass('scrollin');
+          }
       });
-    });
   });
